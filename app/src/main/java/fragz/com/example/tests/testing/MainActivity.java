8a4b4b5d5b6d4c06751b03c30a1dce52710bf80a
BillChabzz <chaby9.8@gmail.com>
2015-10-26 06:47:00 +0300

    created login class and mainactivity content

--- app/src/main/java/fragz/com/example/tests/testing/MainActivity.java
+++ app/src/main/java/fragz/com/example/tests/testing/MainActivity.java
@@ -1,5 +1,6 @@
 package fragz.com.example.tests.testing;
 
+import android.content.pm.PackageInstaller;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,8 +9,15 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
+    UserSessionManager session;
+    Button btnLogout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +34,51 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        session = new UserSessionManager(getApplicationContext());
+
+        TextView lblName = (TextView) findViewById(R.id.lblName);
+        TextView lblEmail = (TextView) findViewById(R.id.lblEmail);
+
+        // Button logout
+        btnLogout = (Button) findViewById(R.id.btnLogout);
+
+        Toast.makeText(getApplicationContext(),
+                "User Login Status: " + session.isUserLoggedIn(),
+                Toast.LENGTH_LONG).show();
+
+
+
+        // Check user login (this is the important point)
+        // If User is not logged in , This will redirect user to LoginActivity
+        // and finish current activity from activity stack.
+        if(session.checkLogin())
+            finish();
+
+        // get user data from session
+        HashMap<String, String> user = session.getUserDetails();
+
+        // get name
+        String name = user.get(UserSessionManager.KEY_NAME);
+
+        // get email
+        String email = user.get(UserSessionManager.KEY_EMAIL);
+
+        // Show user data on activity
+        lblName.setText(Html.fromHtml("Name: <b>" + name + "</b>"));
+        lblEmail.setText(Html.fromHtml("Email: <b>" + email + "</b>"));
+
+
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                // Clear the User session data
+                // and redirect user to LoginActivity
+                session.logoutUser();
+            }
+        });
+    }
     }
 
     @Override
